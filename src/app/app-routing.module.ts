d061c8bc9534469a77f740cd45db7c1a349d80e1
Ele <101021900+GiladitasdL@users.noreply.github.com>
2022-08-08 22:06:15 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducacionComponent } from './educacion/educacion.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:InicioComponent},
+  {path:'Educacion', component:EducacionComponent},
+  {path:'Login', component:LoginComponent},
+  {path:'Proyectos', component:ProyectosComponent},
+  {path:'Skills', component:SkillsComponent},
+  {path:'Sobremi', component:SobremiComponent}  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
