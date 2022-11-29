1fbc59b7ecfd0830660ef380aeadf907383a8f09
Ele Bugna <101021900+GiladitasdL@users.noreply.github.com>
2022-11-29 16:49:38 -0300

    solo falta mi info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from "@angular/common";
+import { RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EducacionComponent } from './educacion/educacion.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducacionComponent,
+    InicioComponent,
+
   ],
   imports: [
+    RouterModule,
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule
--- src/app/educacion/educacion.component.ts
+++ src/app/educacion/educacion.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-educacion',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  educacion = [ 
+    {
+    'id': 1,
+    'name': 'Hola',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    'image': '../../assets/APLogo.png'
+    }, {
+    'id': 2,
+    'name': 'que ',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    'image': '../../assets/APLogo.png'
+    }, {
+    'id': 3,
+    'name': 'puta (:',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    'image': '../../assets/APLogo.png'
+    },
+  ];
 }
